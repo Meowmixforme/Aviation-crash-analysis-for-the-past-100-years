298c41ddc5d6a40bd7a93f88eed6f350f9310e5b
Meowmixforme <v8255920@live.tees.ac.uk>
2024-07-18 09:35:11 +0100

    Begin basic EDA

--- Aviation Crash Analysis.R
+++ Aviation Crash Analysis.R
@@ -3,4 +3,18 @@
 
 # EDA
 
+#install.packages("readr")
+library(readr)
+
+aero <- read.csv("Airplane_Crashes_and_Fatalities_Since_1908.csv")
+
 # Missing data
+
+# Type of data in each column
+
+sapply(aero, class)
+
+misscol <- colSums(is.na(aero)) / nrow(aero)
+round(misscol, 2)
+
+sapply(aero, class)
